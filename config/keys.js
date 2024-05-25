ac1e2670cbf9a5412d4360c9d3c05d5432138854
Asitmondal <mondalasit973@gmail.com>
2024-05-26 03:45:34 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+// const mongoose = require('mongoose');
+// dbPassword = 'mongodb+srv://ExploreSphere:AT775PHUNVdaYnaU@exploresphere.tv5f9g9.mongodb.net/?retryWrites=true&w=majority&appName=ExploreSphere';
+
+// module.exports = {
+//     mongoURI: dbPassword
+// };
+// AT775PHUNVdaYnaU
+//mondalasit0143
+dbPassword = 'mongodb://localhost:27017/e-commerce';
+module.exports = {
+    mongoURI: dbPassword
+};
